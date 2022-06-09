7aac35faa0112b84e45fe5e313ffef3a1d36e708
evoclive <ctdarwell@gmail.com>
2022-06-09 10:27:09 +0700

    Pointless comment #2

--- wolPredictorXtra2_Kmeans_relaxed.V3.py
+++ wolPredictorXtra2_Kmeans_relaxed.V3.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 import re
 import random
 
-#pointless comment fao GitHub test
+#pointless comment fao GitHub test #2
 
 dat = pd.read_csv("geigerDevt.csv", header=0) # working CSV = datMLformatted_geiger2.csv
 taxa = sorted(list(set(dat['NameOnPhylo'])))
